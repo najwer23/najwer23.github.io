e04b447d1cf978d765499babb3cc51b95d7c2ba8
Mariusz Najwer <najwer23@gmail.com>
2022-09-20 13:04:04 +0200

    Social media

--- src/js/ChangeLanguage.js
+++ src/js/ChangeLanguage.js
@@ -1,7 +1,29 @@
 import LanguageContext from "./LanguageContext";
 import { useState, useEffect, useContext } from "react";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faAt, faFilm } from "@fortawesome/free-solid-svg-icons";
+
+
+const objSocialLinks = {
+	"https://github.com/najwer23": <FontAwesomeIcon icon={faGithub} size="2x" />,
+	"https://twitter.com/najwer23": (
+		<FontAwesomeIcon icon={faTwitter} size="2x" style={{ color: "#1DA1F2" }} />
+	),
+	"mailto:najwer23@live.com": <FontAwesomeIcon icon={faAt} size="2x" />,
+
+	"https://www.linkedin.com/in/najwer23/": (
+		<FontAwesomeIcon icon={faLinkedinIn} size="2x" style={{ color: "#0e76a8" }}/>
+	),
+
+	"https://www.filmweb.pl/user/Skold": (
+		<FontAwesomeIcon icon={faFilm} size="2x" style={{ color: "orange" }}/>
+	),
+};
 
 export default function ChangeLanguage(props) {
 	const { language, setLanguage } = useContext(LanguageContext);
@@ -16,6 +38,10 @@ export default function ChangeLanguage(props) {
 
 	function printPDF() {
 		window.print();
+	}
+
+	function openLink(link) {
+		window.open(link, "_blank");
 	}
 
 	return (
@@ -41,13 +67,29 @@ export default function ChangeLanguage(props) {
 				</div>
 
 				<div className="corner-right">
-					<button className="noselect" type="button" onClick={() => printPDF()}>
+					<button
+						className="noselect"
+						type="button"
+						onClick={() => printPDF()}
+						style={{ marginLeft: "20px" }}
+					>
 						<FontAwesomeIcon
 							icon={faFilePdf}
-							size="3x"
+							size="2x"
 							style={{ color: "orangered" }}
 						/>
 					</button>
+
+					{Object.keys(objSocialLinks).map((v, i) => (
+						<button
+							key={i}
+							className="noselect"
+							type="button"
+							onClick={() => openLink(v)}
+						>
+							{objSocialLinks[v]}
+						</button>
+					))}
 				</div>
 			</div>
 		</>
