d0282fa799b73e53091646615940b2bbec7a7a0e
Mariusz Najwer <najwer23@gmail.com>
2022-09-23 20:07:49 +0200

    header

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,10 +1,12 @@
 import Footer from "../footer/Footer";
 import Menu from "../menu/Menu"
+import Title from "./Title"
 
 export default function Main() {
 	return (
 		<>
 			<Menu menuType={"notlogged"} />
+			<Title />
 			<Footer />
 		</>
 	);
---
+++ src/main/Title.js
@@ -0,0 +1,17 @@
+import "./Title.css";
+
+export default function Title() {
+	return (
+		<>
+			<section className="web-header">
+				<div className="web-container">
+					<div className="web-title">
+						<a href="/">
+							<span>Mariusz Najwer</span>
+						</a>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+}
