2a94feb6910f687249c6536efa11deb22d390fac
Mariusz Najwer <najwer23@gmail.com>
2022-09-24 20:11:29 +0200

    Google photos

--- src/grid/GridTemplate.js
+++ src/grid/GridTemplate.js
@@ -5,7 +5,11 @@ import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 
 const POST = [
-	{ path: "/", title: "Barcelona", src: "/img/barcelona/1.jpg" },
+	{
+		path: "/",
+		title: "Barcelona",
+		src: "https://lh3.googleusercontent.com/pw/AL9nZEVlHcQasQcKFb-JhDP2tXGW9zuyhnMaXTNpFDBM8XZ3LSxv5pm6CIt18DDszUWJeVAF8AtqQiXDoxccgSzQaQyxJv4LemLudqJe8e9o_mfhKXQpm8gPEMtazwGvabc-g1X6xkaUWsqWU-MHH9VUzjXd=w723-h964-no?authuser=0",
+	},
 ];
 
 export default function GridTemplate() {
@@ -21,7 +25,8 @@ export default function GridTemplate() {
 								<br />
 								<FontAwesomeIcon icon={faEye} size="x" />
 							</span>
-							<img src={`${process.env.PUBLIC_URL}` + v.src} />
+							{/* <img src={`${process.env.PUBLIC_URL}` + v.src} /> */}
+							<img src={v.src} />
 						</span>
 				</Link>
 			))}
