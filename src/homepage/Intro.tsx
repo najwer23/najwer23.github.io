4d731b33757c4a15b8c4d68a5172cf5d6a467c01
Mariusz Najwer <najwer23@gmail.com>
2023-06-04 18:50:02 +0200

    links

--- src/homepage/Intro.tsx
+++ src/homepage/Intro.tsx
@@ -1,5 +1,5 @@
 import { Button } from "../buttons/Button"
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Intro.css"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,23 +7,40 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
-// import { faAt, faPenNib } from "@fortawesome/free-solid-svg-icons";
+import { faAt } from "@fortawesome/free-solid-svg-icons";
+import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
+
 
 const SOCIAL_MEDIA = [
 	{
 		title: "Github",
 		path: "https://github.com/najwer23",
 		icon: <FontAwesomeIcon icon={faGithub} />,
+		outPage: true
 	},
 	{
 		title: "Twitter",
 		path: "https://twitter.com/najwer23",
 		icon: <FontAwesomeIcon icon={faTwitter} color={"#1DA1F2"}/>,
+		outPage: true
 	},
 	{
 		title: "Linkedin",
 		path: "https://www.linkedin.com/in/najwer23/",
 		icon: <FontAwesomeIcon icon={faLinkedinIn} color={"#0e76a8"}/>,
+		outPage: true
+	},
+	{
+		title: "Mail",
+		path: "/contact",
+		icon: <FontAwesomeIcon icon={faAt} color={"lightgrey"}/>,
+	},
+	{
+		title: "Résumé",
+		path: "https://najwer23.github.io/resume/",
+		icon: <FontAwesomeIcon icon={faFilePdf} color={"orangered"}/>,
+		outPage: true
+
 	},
 ];
 
@@ -43,30 +60,28 @@ export const Intro = () : JSX.Element => {
 				<div className="social-media">
 					{SOCIAL_MEDIA &&
 						SOCIAL_MEDIA.map((v, i) => (
-							<a
-								key={i}
-								href={v.path}
-								rel="noreferrer"
-								title={v.title}
-								target="_blank"
-								className="noselect"
-								aria-label={v.title}
-							>
-								<span className="social-media-item">{v.icon}</span>
-							</a>
+							<span key={i}>
+								{ v.outPage ? (
+									<a
+										key={i}
+										href={v.path}
+										rel="noreferrer"
+										title={v.title}
+										target="_blank"
+										className="noselect"
+										aria-label={v.title}
+									>
+										<span className="social-media-item">{v.icon}</span>
+									</a>
+								) : (
+										<Link title={v.title}  to={v.path} className="noselect">
+										<span className="social-media-item">{v.icon}</span>
+									</Link>
+								)}
+							</span>
 						))}
 				</div>
 
-				<div style={{marginTop: "40px", marginBottom: "40px"}}>
-					<Button
-						type={"button"}
-						onClick={handleClick}
-						className={'noselect snail green'}
-						title={"Connect"}
-						ariaLabel={"Connect"}
-						text={"CONNECT!"}
-					/>
-				</div>
 			</div>
 		</>
 	)
