0c4c79b59bf81ec60d9337cd4f4ea1dc458cab77
Mariusz Najwer <najwer23@gmail.com>
2023-06-08 12:13:44 +0200

    8 days forecast

--- src/weather/Weather.tsx
+++ src/weather/Weather.tsx
@@ -10,6 +10,7 @@ import { useEffect, useState } from 'react'
 import { WeatherStatus } from "../types/typesWeather";
 import Select from 'react-select';
 import { useWindowSize } from "../utils/Utils";
+import Carousel from "../carousel/Carousel";
 
 const townListForSelect = Utils.sortByLabel([
 	{ value: "51.1:17.0333", label: "Wrocław (PL)" },
@@ -24,11 +25,17 @@ const townListForSelect = Utils.sortByLabel([
 export const Weather = (): JSX.Element => {
 	const weatherStatus = useAppSelector((state: RootState) => state.weather.status);
 	const weatherCurrent = useAppSelector((state: RootState) => state.weather.weatherCurrent);
+	const weather8Days = useAppSelector((state: RootState) => state.weather.weather8Days);
 	const dispatch = useAppDispatch();
 	const [width,] = useWindowSize();
+	let forecast8DaysDivs:Array<JSX.Element> = [];
+
+
+
+
+
 
 	const [coords, setCoords] = useState<any>({ value: "51.1:17.0333", label: "Wrocław (PL)" });
-
 	const townListForSelectCSS = {
 		control: (base: any) => ({
 			...base,
@@ -57,10 +64,43 @@ export const Weather = (): JSX.Element => {
 			minWidth: "100%",
 		}),
 	}
-
 	useEffect(() => {
 		dispatch(WeatherSlice.getData(coords.value));
 	}, [dispatch, coords]);
+
+
+
+
+
+
+
+	function makeForecastCarousel(arr:any, arr2:any) {
+		if (!Utils.isEmpty(arr)) {
+			arr.map((x:any, i:number) => (
+				arr2.push(<div key={i} className="forecast-data-current carousel-standard-item">
+					{forecastImgFormater(x.weather)}
+					<ForecastData title={"empty"} value={Utils.dateFormatterFromDt(x.dt)} />
+					<ForecastData title={"Temp"} value={forecastDataFormat(x.temp.day, 4)} />
+					<ForecastData title={"Sunrise"} value={Utils.dateFormatterFromDt(x.sunrise)?.split(",")[1]} />
+					<ForecastData title={"Sunset"} value={Utils.dateFormatterFromDt(x.sunset)?.split(",")[1]} />
+					<ForecastData title={"Wind"} value={forecastDataFormat(x.wind_speed, 1)} />
+					<ForecastData title={"Pressure"} value={forecastDataFormat(x.pressure, 3)} />
+					<ForecastData title={"Humidity"} value={forecastDataFormat(x.humidity, 2)} />
+					<ForecastData title={"Clouds"} value={forecastDataFormat(x.clouds, 2)} />
+
+				</div>)
+			))
+		}
+
+		arr2.shift()
+	}
+	makeForecastCarousel(weather8Days, forecast8DaysDivs);
+
+
+
+
+
+
 
 	return (
 		<>
@@ -82,29 +122,40 @@ export const Weather = (): JSX.Element => {
 				</div>
 
 				<div className="forecast-wrapper">
-					<div className="forecast-data-current-wrapper">
-						{weatherStatus === WeatherStatus.Loading && (
-							<>Loading..</>
-						)}
-						{weatherStatus === WeatherStatus.Done && (
-							<div className="forecast-data-current">
-								{forecastImgFormater(weatherCurrent!.weather)}
-								<ForecastData title={"empty"} value={Utils.dateFormatterFromDt(weatherCurrent!.dt)} />
-								<ForecastData title={"Lat"} value={forecastDataFormat(coords.value?.split(":")[0], 5)} />
-								<ForecastData title={"Lon"} value={forecastDataFormat(coords.value?.split(":")[1], 5)} />
-								<ForecastData title={"Temp"} value={forecastDataFormat(weatherCurrent!.temp, 4)} />
-								<ForecastData title={"Pressure"} value={forecastDataFormat(weatherCurrent!.pressure, 3)} />
-								<ForecastData title={"Humidity"} value={forecastDataFormat(weatherCurrent!.humidity, 2)} />
-								<ForecastData title={"Wind"} value={forecastDataFormat(weatherCurrent!.wind_speed, 1)} />
-								<ForecastData title={"Clouds"} value={forecastDataFormat(weatherCurrent!.clouds, 2)} />
-								<ForecastData title={"Sunrise"} value={Utils.dateFormatterFromDt(weatherCurrent!.sunrise)?.split(",")[1]} />
-								<ForecastData title={"Sunset"} value={Utils.dateFormatterFromDt(weatherCurrent!.sunset)?.split(",")[1]} />
+					{weatherStatus === WeatherStatus.Loading && (
+						<>Loading..</>
+					)}
+					{weatherStatus === WeatherStatus.Done && (
+						<>
+							<div className="forecast-data-current-wrapper">
+								<div className="forecast-data-current">
+									{forecastImgFormater(weatherCurrent!.weather)}
+									<ForecastData title={"empty"} value={Utils.dateFormatterFromDt(weatherCurrent!.dt)} />
+									<ForecastData title={"Temp"} value={forecastDataFormat(weatherCurrent!.temp, 4)} />
+									<ForecastData title={"Sunrise"} value={Utils.dateFormatterFromDt(weatherCurrent!.sunrise)?.split(",")[1]} />
+									<ForecastData title={"Sunset"} value={Utils.dateFormatterFromDt(weatherCurrent!.sunset)?.split(",")[1]} />
+									<ForecastData title={"Wind"} value={forecastDataFormat(weatherCurrent!.wind_speed, 1)} />
+									<ForecastData title={"Pressure"} value={forecastDataFormat(weatherCurrent!.pressure, 3)} />
+									<ForecastData title={"Humidity"} value={forecastDataFormat(weatherCurrent!.humidity, 2)} />
+									<ForecastData title={"Clouds"} value={forecastDataFormat(weatherCurrent!.clouds, 2)} />
+									<ForecastData title={"Lat"} value={forecastDataFormat(coords.value?.split(":")[0], 5)} />
+									<ForecastData title={"Lon"} value={forecastDataFormat(coords.value?.split(":")[1], 5)} />
+								</div>
 							</div>
-						)}
 
-						</div>
-					</div>
-
+							<div className="forecast-data-8Days-wrapper">
+								<Carousel
+									id="carousel2"
+									type="forecast"
+									arr={forecast8DaysDivs}
+									itemName={"carousel-standard-item"}
+									mobileItemWidth={300}
+									desktopItemWidth={300}
+								/>
+							</div>
+						</>
+					)}
+				</div>
 			</div>
 			<Footer />
 		</>
