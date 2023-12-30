2f202fd7cca3b596e2034fbf6771d3a03cc6b086
Mariusz Najwer <najwer23@gmail.com>
2023-12-30 23:25:56 +0100

    Content for asideMenu

---
+++ src/consts.ts
@@ -0,0 +1,71 @@
+interface MenuAsideProps {
+  menuItems: {
+    itemLink: string;
+    itemLinkText: string;
+    itemLinkType: "hash" | "link" | "linkOut";
+  }[];
+  menuTitle: string;
+}
+
+export const MENU_ASIDE: MenuAsideProps = {
+  menuTitle: "Projects",
+  menuItems: [
+    {
+      itemLink: "https://leetcode.com/najwer23/",
+      itemLinkText: "Leetcode",
+      itemLinkType: "linkOut",
+    },
+    {
+      itemLink: "https://najwer23.github.io/react-redux-ts/",
+      itemLinkText: "Geometric figures",
+      itemLinkType: "linkOut",
+    },
+    {
+      itemLink: "https://najwer23.github.io/sudoku/",
+      itemLinkText: "Sudoku",
+      itemLinkType: "linkOut",
+    },
+    {
+      itemLink: "https://jabber.cyclic.app/",
+      itemLinkText: "Jabber",
+      itemLinkType: "linkOut",
+    },
+    {
+      itemLink:
+        "https://najwer23.github.io/najwer23storybook/?path=/docs/getting-started--docs",
+      itemLinkText: "Design system",
+      itemLinkType: "linkOut",
+    },
+    {
+      itemLink: "https://tueuropa.pl/ubezpieczenia-mieszkania-i-domu.htm",
+      itemLinkText: "My house",
+      itemLinkType: "linkOut",
+    },
+    {
+      itemLink: "https://restilo.pl/",
+      itemLinkText: "Restilo",
+      itemLinkType: "linkOut",
+    },
+    {
+      itemLink:
+        "https://tueuropa.pl/ubezpieczenia-sportowe/ubezpieczenie-nnw-sport.htm",
+      itemLinkText: "Active in Sport",
+      itemLinkType: "linkOut",
+    },
+    {
+      itemLink: "https://tueuropa.pl/ubezpieczenia-turystyczne.htm",
+      itemLinkText: "Itravel",
+      itemLinkType: "linkOut",
+    },
+    {
+      itemLink: "https://tvn24.pl/",
+      itemLinkText: "Tvn24",
+      itemLinkType: "linkOut",
+    },
+    {
+      itemLink: "https://tvn.pl/programy/top-model",
+      itemLinkText: "Top model",
+      itemLinkType: "linkOut",
+    },
+  ],
+};
