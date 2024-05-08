3b4a75f320bbe397efedfb793625ee8c0d4ce57c
Mariusz Najwer <najwer23@gmail.com>
2024-05-08 23:08:47 +0200

    Reducing lines

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,17 @@ import { Footer } from "najwer23storybook/lib/Footer";
 import styles from './App.module.css'
 import { MENU_ASIDE } from './App.const';
 
-export const App = () => {
-	return (
-		<div className={styles.container}>
+export const App = () => (
+	<div className={styles.container}>
 
-			<div className={styles.asideMenu}>
-				<MenuAside {...MENU_ASIDE} />
-			</div>
-
-			<div className={styles.routeDiv}>
-				<RouterHash />
-				<Footer />
-			</div>
-
+		<div className={styles.asideMenu}>
+			<MenuAside {...MENU_ASIDE} />
 		</div>
-	)
-}
+
+		<div className={styles.routeDiv}>
+			<RouterHash />
+			<Footer />
+		</div>
+
+	</div>
+)
--- src/pages/home/Nav/Nav.tsx
+++ src/pages/home/Nav/Nav.tsx
@@ -1,20 +1,15 @@
 import { GithubLastMody } from '../../../github/GithubLastMody'
 import styles from './index.module.css'
 
-export const Nav = () => {
-	return (
-		<>
-			<div className={styles.wrapper}>
-				<div className={styles.lastUpdate}>
-					<GithubLastMody repoName="najwer23.github.io" />
-				</div>
-				<div className={styles.resumeLink}>
-					<a target="_blank" href="https://najwer23.github.io/resume/">
-						Résumé
-					</a>
-				</div>
-			</div>
-		</>
-	)
-
-}
+export const Nav = () => (
+	<div className={styles.wrapper}>
+		<div className={styles.lastUpdate}>
+			<GithubLastMody repoName="najwer23.github.io" />
+		</div>
+		<div className={styles.resumeLink}>
+			<a target="_blank" href="https://najwer23.github.io/resume/">
+				Résumé
+			</a>
+		</div>
+	</div>
+)
