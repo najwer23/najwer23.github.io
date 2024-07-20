905b8f47a8f81131f7ec28e856f5fa5c3386f035
Mariusz Najwer <najwer23@gmail.com>
2024-07-20 09:43:19 +0200

    Weather Pciture as Picture component

--- src/app/pages/weather/WeatherImg.tsx
+++ src/app/pages/weather/WeatherImg.tsx
@@ -1,18 +1,19 @@
 import { isEmpty } from '@najwer23/functions/isEmpty';
+import { Picture } from 'najwer23storybook/lib/Picture';
 
 export const WeatherImg = ({ imgArr }: { imgArr: { icon: string }[] }) => {
   if (isEmpty(imgArr)) {
     return <div style={{ width: '100px', height: '100px', marginBottom: '20px' }}> </div>;
   } else {
     return (
-      <img
-        draggable="false"
-        width="100px"
-        height="100px"
-        alt="weather img"
-        style={{ background: 'burlywood', marginBottom: '20px' }}
-        src={`https://openweathermap.org/img/wn/${imgArr[0].icon}@2x.png`}
-      />
+      <div style={{ background: 'burlywood', marginBottom: '20px', width: '100px', height: '100px' }}>
+        <Picture
+          src={`https://openweathermap.org/img/wn/${imgArr[0].icon}@2x.png`}
+          alt={`${imgArr[0].icon}`}
+          ar={1}
+          draggable={false}
+        />
+      </div>
     );
   }
 };
