6d95d4a3c12875f7dfd662715e50fce9ad0bdffd
Mariusz Najwer <najwer23@gmail.com>
2024-08-19 11:51:29 +0200

    GTM - test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,8 +5,21 @@ import { MenuAside } from 'najwer23storybook/lib/MenuAside';
 import styles from './App.module.css';
 import { MENU_ASIDE } from './App.const';
 
-export const App = () => (
-  <div className={styles.container}>
+import ReactGA from 'react-ga4';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+ReactGA.initialize('GTM-MQF3H8WG');
+
+export const App = () => {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location])
+
+  return <div className={styles.container}>
     <div className={styles.asideMenu}>
       <MenuAside {...MENU_ASIDE} />
     </div>
@@ -15,4 +28,4 @@ export const App = () => (
       <RouterHash />
     </div>
   </div>
-);
+};
