f0325397087c60bcd92f9cd71b37d6eaf37e89d7
Mariusz Najwer <najwer23@gmail.com>
2024-12-25 15:11:51 +0100

    New projects site

---
+++ src/app/pages/projects/Projects.const.ts
@@ -0,0 +1,175 @@
+export const PROJECTS = [
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/35/01/8e/35018e10c4b5b537e1044c7f62c4e968.jpg',
+    name: 'Design System',
+    to: 'https://najwer23.github.io/najwer23snacks',
+    outside: true,
+    projectDesc:
+      'A design system is a comprehensive collection of reusable components, guidelines, and standards that helps teams create consistent and cohesive digital products. It serves as a single source of truth, streamlining design and development processes across various platforms and teams. Key features include reusable UI components, thorough documentation, style guides, and design tokens. The benefits of using a design system are substantial, leading to increased efficiency by reducing time spent on repetitive tasks. It also ensures consistency across products, enhancing brand recognition and user experience. Additionally, design systems improve collaboration among stakeholders and allow for scalability and quality control as products evolve.',
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/99/23/2b/99232bf6687536d05f7ed806bac5c8c1.jpg',
+    name: 'Indio',
+    to: 'https://www.useindio.com/',
+    outside: true,
+    projectDesc:
+      'Indio is an online platform designed to streamline the insurance application and renewal process for agencies and their clients. It combines various functions into a single, secure interface, allowing users to access and manage insurance forms anytime and anywhere. One of its key features is "smart mapping," which automatically populates data across multiple forms, reducing the need for repetitive data entry. Indio also offers a vast library of over 10,000 applications and insurance forms, enhancing efficiency and accuracy in data gathering. The platform supports collaborative interactions, enabling clients and agents to communicate directly within applications, thus minimizing errors and omissions. Additionally, Indio ensures high levels of security for sensitive data through encryption and regular audits.',
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/1d/28/74/1d2874c825e26cab7dd599ec12cdef96.jpg',
+    name: 'Gym',
+    to: 'https://najwer23gym.vercel.app',
+    outside: true,
+    projectDesc:
+      'This project focuses on developing a comprehensive fitness training application designed to help users achieve their fitness goals through structured workout plans. The primary training regimen includes ambitious targets of 100 push-ups, 300 squats, 300 sit-ups, and 40 pull-ups, providing users with a clear path to enhance their strength and endurance.',
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/87/e9/52/87e9528a00977827fcb4a2020a0bb40f.jpg',
+    name: 'Applied Systems',
+    to: 'https://www1.appliedsystems.com/en-us/',
+    outside: true,
+    projectDesc:
+      'Applied Systems is a prominent global provider of cloud-based software specifically designed for the insurance industry. Established in 1983 and headquartered in Chicago, Illinois, the company has become a pioneer in insurance automation and is recognized as the largest agency and brokerage management systems provider worldwide. Its solutions are utilized by over 339 000 users across various regions, including the United States, Canada, Ireland, and the United Kingdom.',
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/d0/64/03/d064039136bea1bbf72c873cf29e3bae.jpg',
+    name: 'TVN. Top model 2023',
+    to: 'https://tvn.pl/programy/top-model',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/6f/a0/1d/6fa01d32f31e77a0dd5f7c21620f562b.jpg',
+    name: 'TVN. Electronic program guide',
+    to: 'https://tvn.pl/program-tv',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/c6/45/6b/c6456b0a8f2bf830df73f3cb206e8645.jpg',
+    name: 'TVN Warszawa',
+    to: 'https://tvn24.pl/tvnwarszawa',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/0a/25/37/0a2537ba934104f730b3fb9da5d64f3c.jpg',
+    name: 'TVN24',
+    to: 'https://tvn24.pl/',
+    outside: true,
+    projectDesc:
+      'TVN24.pl is a leading Polish news portal that provides comprehensive coverage of current events, both domestically and internationally. It is part of the TVN Group, which is owned by Warner Bros. Discovery, and is known for its round-the-clock news programming.',
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/34/3e/4b/343e4bf9af8fc4915d272c2cb6189b1c.jpg',
+    name: 'TVN24. Stock quotes',
+    to: 'https://tvn24.pl/biznes/notowania/waluty',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/90/ff/ce/90ffce303b5496ac8f253210fd0bef55.jpg',
+    name: 'TVN24. Polish Local Government Elections 2024',
+    to: 'https://tvn24.pl/wybory-samorzadowe-2024',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/3d/e4/96/3de496514ffd1bf919cd67acb6a95ba7.jpg',
+    name: 'TVN24. European Elections 2024',
+    to: 'https://tvn24.pl/wybory-do-europarlamentu-2024',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/89/c4/7d/89c47d05a0dfc1ca3256f5060b2e463a.jpg',
+    name: 'Restilo',
+    to: 'https://restilo.pl/',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/736x/9f/1e/49/9f1e49e099d06dd3fc2b1e05eb22cb37.jpg',
+    name: 'My house',
+    to: 'https://tueuropa.pl/ubezpieczenia-mieszkania-i-domu.htm',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/0a/87/ba/0a87ba8171512a5f95c635dccb6aca01.jpg',
+    name: 'Active in sport',
+    to: 'https://tueuropa.pl/ubezpieczenia-sportowe/ubezpieczenie-nnw-sport.htm',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/b3/06/50/b306508e64ec001d07899cf897538609.jpg',
+    name: 'My Health',
+    to: 'https://tueuropa.pl/ubezpieczenia-zycia-i-zdrowia/ubezpieczenie-twojego-zdrowia-myhealth.htm',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/736x/29/e1/ad/29e1ada74c0ed416406d01c3f1eb626d.jpg',
+    name: 'Itravel',
+    to: 'https://tueuropa.pl/ubezpieczenia-turystyczne.htm',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/26/bf/09/26bf09f70b9cfefea3f8d6f7377dc604.jpg',
+    name: 'Geometric figures',
+    to: 'https://najwer23.github.io/react-redux-ts',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/cc/16/f2/cc16f234f2403d38e8e466be678ac06f.jpg',
+    name: 'Sudoku',
+    to: 'https://najwer23.github.io/sudoku/',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/47/54/30/475430b668d7acb8e3a5b6155164a767.jpg',
+    name: 'Weather app',
+    to: '/weather',
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/ab/85/f9/ab85f911b3966b0bb6b04cbec383253a.jpg',
+    name: 'Stock Quotes',
+    to: '/stock-quotes',
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/9c/16/ec/9c16ec06c397804495d596eaabb90e83.jpg',
+    name: 'Fastify API',
+    to: 'https://najwer23api-fastify.vercel.app/',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/99/3e/5b/993e5b60ad50ccdd9bb1a7943727577e.jpg',
+
+    name: 'Résumé',
+    to: 'https://najwer23.github.io/resume/',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/7d/38/41/7d384174fb91bafe0270c1cd6f4cfa22.jpg',
+    name: 'The traveling salesman problem with Simulated Annealing',
+    to: 'https://najwer23.github.io/tsp/',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/f3/20/27/f320273f22cbf8ee4e2544b8cd3e0596.jpg',
+    name: "Valentine's Day Cards",
+    to: 'https://najwer23.github.io/flip-card',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/41/05/30/4105309028fe700e5af1ddf3a79d52c9.jpg',
+    name: 'Reed–Solomon error correction',
+    to: 'https://najwer23.github.io/bch-code-js/',
+    outside: true,
+  },
+  {
+    pictureUrl: 'https://i.pinimg.com/1200x/36/21/8a/36218ad179afc7c6f8de6afcdc86cbae.jpg',
+    name: 'Astronomy Picture Of the Day',
+    to: '/apod',
+  },
+  { 
+    pictureUrl: "https://i.pinimg.com/1200x/1d/a5/b2/1da5b279c4e05b76c78c2d0a8d1f3452.jpg",
+    name: 'Masonry layout', to: '/gallery' },
+  {
+    pictureUrl: "https://i.pinimg.com/1200x/f3/96/52/f3965260cf7a42ad9610c474b2ef7f38.jpg",
+    name: `Leetcode problems solved: `,
+    to: 'https://leetcode.com/u/najwer23',
+    outside: true,
+  },
+];
--- src/app/pages/projects/Projects.tsx
+++ src/app/pages/projects/Projects.tsx
@@ -4,11 +4,22 @@ import { useDocumentTitle } from '@najwer23/utils/hooks/useDocumentTitle';
 import { RouterLinks } from '@najwer23/app/router/RouterLinks';
 import { queryLeetcode } from './Projects.query';
 import { useQuery } from '@tanstack/react-query';
+import { Dialog } from 'najwer23snacks/lib/Dialog';
+import { useState } from 'react';
+import { Picture } from 'najwer23snacks/lib/Picture';
+import styles from './Projects.module.css';
+import { PROJECTS } from './Projects.const';
 
 export const Projects: React.FC<{
   title: string;
 }> = ({ title }) => {
   useDocumentTitle(title);
+
+  const [dialog, setDialog] = useState<{ isOpen: boolean; src: string; alt: string }>({
+    isOpen: false,
+    src: PROJECTS[0].pictureUrl ?? "",
+    alt: '',
+  });
 
   const { data, isLoading } = useQuery({
     queryKey: ['queryLeetcode', 'queryLeetcode'],
@@ -23,95 +34,65 @@ export const Projects: React.FC<{
     <Grid widthMax={1400} layout="container" padding="10px 10px 10px 10px" margin="auto">
       <TextBox tag="h1"> Projects</TextBox>
 
-      {[
-        {
-          name: 'Design System',
-          to: 'https://najwer23.github.io/najwer23snacks',
-          outside: true,
-          projectDesc:
-            'A design system is a comprehensive collection of reusable components, guidelines, and standards that helps teams create consistent and cohesive digital products. It serves as a single source of truth, streamlining design and development processes across various platforms and teams. Key features include reusable UI components, thorough documentation, style guides, and design tokens. The benefits of using a design system are substantial, leading to increased efficiency by reducing time spent on repetitive tasks. It also ensures consistency across products, enhancing brand recognition and user experience. Additionally, design systems improve collaboration among stakeholders and allow for scalability and quality control as products evolve.',
-        },
-        {
-          name: 'Indio',
-          to: 'https://www.useindio.com/',
-          outside: true,
-          projectDesc:
-            'Indio is an online platform designed to streamline the insurance application and renewal process for agencies and their clients. It combines various functions into a single, secure interface, allowing users to access and manage insurance forms anytime and anywhere. One of its key features is "smart mapping," which automatically populates data across multiple forms, reducing the need for repetitive data entry. Indio also offers a vast library of over 10,000 applications and insurance forms, enhancing efficiency and accuracy in data gathering. The platform supports collaborative interactions, enabling clients and agents to communicate directly within applications, thus minimizing errors and omissions. Additionally, Indio ensures high levels of security for sensitive data through encryption and regular audits.',
-        },
-        {
-          name: 'Gym',
-          to: 'https://najwer23gym.vercel.app',
-          outside: true,
-          projectDesc:
-            'This project focuses on developing a comprehensive fitness training application designed to help users achieve their fitness goals through structured workout plans. The primary training regimen includes ambitious targets of 100 push-ups, 300 squats, 300 sit-ups, and 40 pull-ups, providing users with a clear path to enhance their strength and endurance.',
-        },
-        { name: 'Applied Systems', to: 'https://www1.appliedsystems.com/en-us/', outside: true, projectDesc: "Applied Systems is a prominent global provider of cloud-based software specifically designed for the insurance industry. Established in 1983 and headquartered in Chicago, Illinois, the company has become a pioneer in insurance automation and is recognized as the largest agency and brokerage management systems provider worldwide. Its solutions are utilized by over 339 000 users across various regions, including the United States, Canada, Ireland, and the United Kingdom." },
-        { name: 'TVN. Top model 2023', to: 'https://tvn.pl/programy/top-model', outside: true },
-        { name: 'TVN. Electronic program guide', to: 'https://tvn.pl/program-tv', outside: true },
-        { name: 'TVN Warszawa', to: 'https://tvn24.pl/tvnwarszawa', outside: true },
-        { name: 'TVN24', to: 'https://tvn24.pl/', outside: true, projectDesc: "TVN24.pl is a leading Polish news portal that provides comprehensive coverage of current events, both domestically and internationally. It is part of the TVN Group, which is owned by Warner Bros. Discovery, and is known for its round-the-clock news programming." },
-        { name: 'TVN24. Stock quotes', to: 'https://tvn24.pl/biznes/notowania/waluty', outside: true },
-        {
-          name: 'TVN24. Polish Local Government Elections 2024',
-          to: 'https://tvn24.pl/wybory-samorzadowe-2024',
-          outside: true,
-        },
-        {
-          name: 'TVN24. European Elections 2024',
-          to: 'https://tvn24.pl/wybory-do-europarlamentu-2024',
-          outside: true,
-        },
-        { name: 'Restilo', to: 'https://restilo.pl/', outside: true },
-        { name: 'My house', to: 'https://tueuropa.pl/ubezpieczenia-mieszkania-i-domu.htm', outside: true },
-        {
-          name: 'Active in sport',
-          to: 'https://tueuropa.pl/ubezpieczenia-sportowe/ubezpieczenie-nnw-sport.htm',
-          outside: true,
-        },
-        {
-          name: 'My Health',
-          to: 'https://tueuropa.pl/ubezpieczenia-zycia-i-zdrowia/ubezpieczenie-twojego-zdrowia-myhealth.htm',
-          outside: true,
-        },
-        { name: 'Itravel', to: 'https://tueuropa.pl/ubezpieczenia-turystyczne.htm', outside: true },
-        { name: 'Geometric figures', to: 'https://najwer23.github.io/react-redux-ts', outside: true },
-        { name: 'Sudoku', to: 'https://najwer23.github.io/sudoku/', outside: true },
-        { name: 'Weather app', to: '/weather' },
-        { name: 'Stock Quotes', to: '/stock-quotes' },
-        { name: 'Fastify API', to: 'https://najwer23api-fastify.vercel.app/', outside: true },
-        { name: 'Résumé', to: 'https://najwer23.github.io/resume/', outside: true },
-        {
-          name: 'The traveling salesman problem with Simulated Annealing',
-          to: 'https://najwer23.github.io/tsp/',
-          outside: true,
-        },
-        { name: "Valentine's Day Cards", to: 'https://najwer23.github.io/flip-card', outside: true },
-        { name: 'Reed–Solomon error correction', to: 'https://najwer23.github.io/bch-code-js/', outside: true },
-        { name: 'Astronomy Picture Of the Day', to: '/apod' },
-        { name: 'Masonry layout', to: '/gallery' },
-        {
-          name: `Leetcode problems solved: ${
-            isLoading
-              ? '1000+'
-              : // : Number(
-                //     data?.najwer23.submitStatsGlobal.acSubmissionNum.find((item) => item.difficulty === 'All')?.count,
-                //   ) + 18
-                (data?.totalSolved ?? '')
-          }`,
-          to: 'https://leetcode.com/u/najwer23',
-          outside: true,
-        },
-      ].map((v) => (
-        <>
-          <RouterLinks routes={[v]} tag="h3" key={v.name} />
-
-          {v.projectDesc && (
-            <Grid widthMax={700} layout="container" padding="0 0 10px" margin="0">
-              <TextBox tag="p"> {v.projectDesc}</TextBox>
-            </Grid>
-          )}
-        </>
+      {PROJECTS.map((v, i) => (
+        <Grid
+          key={v.name}
+          layout={'columns'}
+          margin="0"
+          padding="0 0 10px"
+          gap={{
+            col: '100px',
+            row: '0px',
+          }}
+          col={{
+            mobile: 1,
+            smallDesktop: 2,
+            desktop: 2,
+            tablet: 2,
+          }}>
+          <div>
+            {v.name.includes('Leetcode') ? (
+              <RouterLinks
+                routes={[
+                  {
+                    ...v,
+                    name: v.name + (isLoading
+                      ? '1000+'
+                      : // Number(
+                        //   data?.najwer23.submitStatsGlobal.acSubmissionNum.find((item) => item.difficulty === 'All')?.count,
+                        // ) + 18
+                        (data?.totalSolved ?? '')),
+                  },
+                ]}
+                tag="h3"
+                key={v.name}
+              />
+            ) : (
+              <RouterLinks routes={[v]} tag="h3" key={v.name} />
+            )}
+            {v.projectDesc && (
+              <div>
+                <TextBox tag="p"> {v.projectDesc}</TextBox>
+              </div>
+            )}
+          </div>
+          <Grid widthMax={1400} layout="container" padding="0px 0 30px 0" margin="0" key={`child${v.name}}`}>
+            {v.pictureUrl && (
+              <div
+                onClick={() => setDialog({ isOpen: true, src: v.pictureUrl, alt: v.name })}
+                className={styles.pictureWrapper}>
+                <Picture src={v.pictureUrl} alt={title} key={i} kind="border" />
+              </div>
+            )}
+          </Grid>
+        </Grid>
       ))}
+
+      <Dialog modalOpen={dialog.isOpen} modalClose={() => setDialog({ ...dialog, isOpen: false })}>
+        <div>
+          <Picture src={dialog.src} alt={dialog.alt} />
+        </div>
+      </Dialog>
     </Grid>
   );
 };
