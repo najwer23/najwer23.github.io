55e5a0021f1928ce619c760e77bde382043149d6
Mariusz Najwer <najwer23@gmail.com>
2025-06-08 21:02:08 +0200

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,42 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // devOptions: {
+      //   enabled: true,
+      // },
+      manifest: {
+        name: 'Mariusz Najwer',
+        short_name: 'Mariusz Najwer',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'icon/192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'icon/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     outDir: './build',
 
