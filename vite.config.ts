73cd768a648694a0ca8e03195dd8dae47e3e8c2b
Mariusz Najwer <najwer23@gmail.com>
2025-06-08 22:11:39 +0200

    PWA only on subpath

--- vite.config.ts
+++ vite.config.ts
@@ -7,36 +7,33 @@ import path from 'path';
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      scope: '/', 
-      registerType: 'autoUpdate',
-      // devOptions: {
-      //   enabled: true,
-      // },
-      manifest: {
-        name: 'Mariusz Najwer',
-        short_name: 'Mariusz Najwer',
-        display: 'standalone',
-        start_url: '/',
-        scope: '/',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        icons: [
-          {
-            src: 'icon/192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any',
-          },
-          {
-            src: 'icon/512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any',
-          },
-        ],
-      },
-    }),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   // devOptions: {
+    //   //   enabled: true,
+    //   // },
+    //   manifest: {
+    //     name: 'Mariusz Najwer',
+    //     short_name: 'Mariusz Najwer',
+    //     display: 'standalone',
+    //     background_color: '#ffffff',
+    //     theme_color: '#000000',
+    //     icons: [
+    //       {
+    //         src: 'icon/192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png',
+    //         purpose: 'any',
+    //       },
+    //       {
+    //         src: 'icon/512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //         purpose: 'any',
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
   build: {
     outDir: './build',
