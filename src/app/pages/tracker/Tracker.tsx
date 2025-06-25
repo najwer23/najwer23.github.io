a5d70de1b936e4af8a5ef490e3c47c962deb66c7
Mariusz Najwer <najwer23@gmail.com>
2025-06-25 23:37:03 +0200

    Navigation mobile and fontawesome icons

--- src/app/pages/tracker/Tracker.tsx
+++ src/app/pages/tracker/Tracker.tsx
@@ -8,6 +8,8 @@ import { TextBox } from 'najwer23snacks/lib/Textbox';
 import { MapContainer, TileLayer, Polyline, CircleMarker } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHourglassHalf, faRoute } from '@fortawesome/free-solid-svg-icons';
 
 export const Tracker: React.FC<{ title: string }> = ({ title }) => {
   useDocumentTitle(title);
@@ -34,8 +36,6 @@ export const Tracker: React.FC<{ title: string }> = ({ title }) => {
     return `${hh} / ${mm} / ${ss}`;
   };
 
-  console.log(result);
-
   return (
     <Grid widthMax={1400} layout="container" padding="10px" margin="auto">
       {isLoading && <Spinner />}
@@ -45,7 +45,7 @@ export const Tracker: React.FC<{ title: string }> = ({ title }) => {
           <>
             <TextBox tag="h1">Tracker sessions</TextBox>
 
-            <Grid widthMax={1400} layout="container" padding="0" margin="auto">
+            <Grid widthMax={1400} layout="container" padding="0" margin="auto auto -100px">
               {result?.data.items.length == 0 && <TextBox tag="p">There are currently no sessions :(</TextBox>}
 
               {result?.data.items.map((v, i) => (
@@ -53,18 +53,18 @@ export const Tracker: React.FC<{ title: string }> = ({ title }) => {
                   key={`tracker-item-${i}`}
                   widthMax={1400}
                   layout="container"
-                  padding="10px 0 30px 0"
+                  padding="10px 0 100px 0"
                   margin="auto">
                   <Grid
                     layout="columns"
-                    gap={{ col: '100px', row: '0px' }}
+                    gap={{ col: '100px', row: '15px' }}
                     col={{ mobile: 1, smallDesktop: 2, desktop: 2, tablet: 2 }}>
                     <div>
                       <MapContainer
                         center={[v.locationsList[0].lat, v.locationsList[0].lon]}
                         zoom={18}
                         scrollWheelZoom={false}
-                        style={{ height: '600px', width: '100%' }}>
+                        style={{ height: '400px', width: '100%' }}>
                         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                         <Polyline
                           positions={v.locationsList.map((x) => [x.lat, x.lon])}
@@ -91,8 +91,23 @@ export const Tracker: React.FC<{ title: string }> = ({ title }) => {
                       </MapContainer>
                     </div>
                     <div>
-                      <TextBox tag="p">Distance: {(v.totalDistance / 1000).toFixed(3)} km</TextBox>
-                      <TextBox tag="p">{formatMsToHHMM(v.duration)}</TextBox>
+                      <Grid
+                        layout="columns"
+                        gap={{ col: '10px', row: '0px' }}
+                        col={{ mobile: 2, smallDesktop: 2, desktop: 2, tablet: 2 }}>
+                        <div>
+                          <TextBox tag="p">
+                            <FontAwesomeIcon icon={faRoute} color="white" size={'2x'} />
+                          </TextBox>
+                          <TextBox tag="h3">{(v.totalDistance / 1000).toFixed(3)} km</TextBox>
+                        </div>
+                        <div>
+                          <TextBox tag="p">
+                            <FontAwesomeIcon icon={faHourglassHalf} color="white" size={'2x'} />
+                          </TextBox>
+                          <TextBox tag="h3">{formatMsToHHMM(v.duration)}</TextBox>
+                        </div>
+                      </Grid>
                     </div>
                   </Grid>
                 </Grid>
