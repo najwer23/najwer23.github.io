b5b5b3785b9c8460294814fe90317ab0c8fb4d77
Mariusz Najwer <najwer23@gmail.com>
2025-11-10 17:54:59 +0100

    Remove Tracker and Weather pages along with their associated queries, styles, and components to streamline the application. Update router configuration to reflect these changes and clean up unused CSS styles and components.

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,30 +1,9 @@
-import { Footer } from 'najwer23snacks/lib/Footer';
-import { Navigation } from 'najwer23snacks/lib/Navigation';
 import { Outlet } from 'react-router-dom';
-import { RouterLinks } from '../router/RouterLinks';
-import { LAYOUT_NAVIGATION } from './Layout.const';
-import styles from './Layout.module.css';
-import { type LayoutNavigation } from './Layout.types';
 
-export const Layout: React.FC<{
-  menu?: keyof LayoutNavigation;
-}> = ({ menu = 'notLoggedIn' }) => {
+export const Layout: React.FC = () => {
   return (
     <>
-      <Navigation
-        navigationTitle={
-          <RouterLinks routes={[{ name: 'Mariusz Najwer', to: '/', isTitle: true }]} tag="h2" className={styles.logo} />
-        }
-        navigationTop={<>{LAYOUT_NAVIGATION[menu].navigationTop}</>}
-        navigationMobile={<>{LAYOUT_NAVIGATION[menu].navigationMobile}</>}
-        navigationMobileAtWidth={768}
-        navigationBottom={<>{LAYOUT_NAVIGATION[menu].navigationBottom}</>}
-        hrColor1={LAYOUT_NAVIGATION[menu].hrColor1}
-        hrColor2={LAYOUT_NAVIGATION[menu].hrColor2}
-        navigationWidthMax={1400}
-      />
       <Outlet />
-      <Footer footerWidthMax={1400} />
     </>
   );
 };
--- src/app/router/RouterHash.tsx
+++ src/app/router/RouterHash.tsx
@@ -1,14 +1,6 @@
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { Layout } from '../layout/Layout';
-import { Apod } from '../pages/apod/Apod';
-import { Contact } from '../pages/contact/Contact';
-import { ErrorNotFound } from '../pages/error/ErrorNotFound';
-import { Gallery } from '../pages/gallery/Gallery';
 import { Home } from '../pages/home/Home';
-import { Projects } from '../pages/projects/Projects';
-import { StockQuotes } from '../pages/stock-quotes/StockQuotes';
-import { Tracker } from '../pages/tracker/Tracker';
-import { Weather } from '../pages/weather/Weather';
 import { ScrollToTop } from './ScrollToTop';
 
 const BASE_TITLE = ' | Mariusz Najwer';
@@ -19,7 +11,7 @@ const router = createHashRouter([
     element: (
       <>
         <ScrollToTop />
-        <Layout menu="notLoggedIn" />
+        <Layout />
       </>
     ),
     children: [
@@ -30,42 +22,6 @@ const router = createHashRouter([
       {
         path: '/home',
         element: <Home title={`Home${BASE_TITLE}`} />,
-      },
-      {
-        path: '/contact',
-        element: <Contact title={`Contact${BASE_TITLE}`} />,
-      },
-      {
-        path: '/projects',
-        element: <Projects title={`Projects${BASE_TITLE}`} />,
-      },
-      {
-        path: '/tracker',
-        element: <Tracker title={`Tracker${BASE_TITLE}`} />,
-      },
-      {
-        path: '/weather',
-        element: <Weather title={`Weather${BASE_TITLE}`} />,
-      },
-      {
-        path: '/gallery',
-        element: <Gallery title={`Gallery${BASE_TITLE}`} />,
-      },
-      {
-        path: '/stock-quotes',
-        element: <StockQuotes title={`Stock Quotes${BASE_TITLE}`} />,
-      },
-      ...['/apod/page/:page', '/apod'].map((path) => ({
-        path,
-        element: (
-          <>
-            <Apod title={`Picture of the day${BASE_TITLE}`} />
-          </>
-        ),
-      })),
-      {
-        path: '*',
-        element: <ErrorNotFound title={`Error not found${BASE_TITLE}`} />,
       },
     ],
   },
