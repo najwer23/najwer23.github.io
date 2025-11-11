7d1548d6b750f10f6fa410b0015548c19575e890
Mariusz Najwer <najwer23@gmail.com>
2025-11-11 14:33:08 +0100

    Sample projects

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -20,7 +20,6 @@ export const Home: React.FC<{}> = () => {
           </TextBox>
         </Grid>
       </Grid>
-
       <Grid layout="container" widthMax={1360} padding={'0'}>
         <div style={{ height: '400px', width: '100%' }}>
           <SliderScroll gap="10px" isCircular autoPlay autoPlaySpeed={0.5}>
@@ -52,7 +51,6 @@ export const Home: React.FC<{}> = () => {
           </SliderScroll>
         </div>
       </Grid>
-
       <Grid layout="container" widthMax={1400} padding={'20px 20px 40px 20px'}>
         <Grid layout="container" padding={'100px 0 50px 0'} margin={0}>
           <Grid
@@ -105,7 +103,6 @@ export const Home: React.FC<{}> = () => {
           </Grid>
         </Grid>
       </Grid>
-
       <Grid layout="container" widthMax={1360} padding={'0 0 40px 0'} className={styles.homeSliderItemsDesktop}>
         <div
           style={{
@@ -157,7 +154,6 @@ export const Home: React.FC<{}> = () => {
           </Slider>
         </div>
       </Grid>
-
       <Grid layout="container" widthMax={1360} padding={'0 0 40px 0'} className={styles.homeSliderItemsMobile}>
         <div
           style={{
@@ -196,6 +192,86 @@ export const Home: React.FC<{}> = () => {
             </div>
           </Slider>
         </div>
+      </Grid>
+
+      <Grid layout="container" widthMax={1400} padding={'40px 20px 0px 20px'}>
+        <Grid layout="container" widthMax={700} padding={'0 0 40px 0'} margin={0}>
+          <TextBox tag="h2" desktopSize={50} mobileSize={40} fontWeight={500}>
+            Sample projects
+          </TextBox>
+        </Grid>
+      </Grid>
+
+      <Grid layout="container" widthMax={1400} padding={'0px 20px 20px 20px'}>
+        <Grid layout="container" widthMax={700} padding={'0 0 10px 0'} margin={0}>
+          <TextBox tag="h2" desktopSize={30} mobileSize={24} fontWeight={500}>
+            APPLIED SYSTEMS INC.
+          </TextBox>
+        </Grid>
+        <Grid layout="container" widthMax={900} padding={0} margin={0}>
+          <TextBox tag="p" desktopSize={18} mobileSize={16} fontWeight={400}>
+            My responsibility was to create a frontend application enabling the purchase of insurance through a website.
+            The application was developed using React.js and TypeScript, leveraging the Applied Systems design system.
+            The primary technical challenge was implementing a recursive Depth-First Search (DFS) algorithm to
+            conditionally render a nested, graph-like structure of questions, where each question’s visibility depended
+            on previous answers. The question data was fetched from the backend as a large JSON payload (around 60,000
+            lines), structured as a Server Driven UI model. The development was part of a 8-person international team
+            spanning Poland, India, Canada, England, the USA, and Australia. In addition to the core questionnaire, I
+            contributed to building an admin panel, implementing user action tracking scripts, creating statistical
+            charts, and ensuring compliance with WCAG 2.1 AA accessibility standards.
+          </TextBox>
+        </Grid>
+      </Grid>
+
+      <Grid layout="container" widthMax={1400} padding={'40px 20px 20px 20px'}>
+        <Grid layout="container" widthMax={700} padding={'0 0 10px 0'} margin={0}>
+          <TextBox tag="h2" desktopSize={30} mobileSize={24} fontWeight={500}>
+            WARNER BROS. DISCOVERY, TVN24.pl
+          </TextBox>
+        </Grid>
+        <Grid layout="container" widthMax={900} padding={0} margin={0}>
+          <TextBox tag="p" desktopSize={18} mobileSize={16} fontWeight={400}>
+            My responsibility was to develop a new version of the TVN24.pl website from the ground up using SSR
+            technology. The architecture was inspired by the islands concept popularized by Astro.js, featuring
+            microfrontend applications implemented with React.js and Web Components. As part of the development team, I
+            contributed to building multiple interactive widgets, including stock quotes, weather forecasts, polls,
+            quizzes, election tools, TV program guides, and a custom SSR-based microfrontend rendering algorithm that
+            operates without relying on external libraries.
+          </TextBox>
+        </Grid>
+      </Grid>
+
+      <Grid layout="container" widthMax={1400} padding={'40px 20px 20px 20px'}>
+        <Grid layout="container" widthMax={700} padding={'0 0 10px 0'} margin={0}>
+          <TextBox tag="h2" desktopSize={30} mobileSize={24} fontWeight={500}>
+            FLHF S.A
+          </TextBox>
+        </Grid>
+        <Grid layout="container" widthMax={900} padding={0} margin={0}>
+          <TextBox tag="p" desktopSize={18} mobileSize={16} fontWeight={400}>
+            My responsibility was to enhance a website initially developed by a software house while simultaneously
+            refreshing its graphic design. The core focus was to develop ecommerce-specific components such as sliders,
+            carousels, product listings, search functionality, and a shopping cart. Key priorities included optimizing
+            for mobile view, improving SEO, descriptive markup and metadata, and enhancing performance metrics such as
+            Cumulative Layout Shift (CLS), page speed, and accessibility, measured and optimized using Lighthouse.
+          </TextBox>
+        </Grid>
+      </Grid>
+
+      <Grid layout="container" widthMax={1400} padding={'40px 20px 20px 20px'}>
+        <Grid layout="container" widthMax={700} padding={'0 0 10px 0'} margin={0}>
+          <TextBox tag="h2" desktopSize={30} mobileSize={24} fontWeight={500}>
+            EUROPA UBEZPIECZENIA
+          </TextBox>
+        </Grid>
+        <Grid layout="container" widthMax={900} padding={'0 0 100px 0'} margin={0}>
+          <TextBox tag="p" desktopSize={18} mobileSize={16} fontWeight={400}>
+            My duty was to maintain and develop an internal system for managing insurance policies. The system was used
+            by insurance agents and office workers. My tasks included creating new modules, integrating external systems
+            (online payments, client data verification, insurance application creation), and optimizing application
+            performance. I worked on both the frontend (Angular) and backend (Java, Groovy, SQL) of the application.
+          </TextBox>
+        </Grid>
       </Grid>
     </>
   );
