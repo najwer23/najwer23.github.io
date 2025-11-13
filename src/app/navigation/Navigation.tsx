4915f4736ea2959f5d7b1450f3ac2c9e91d1714b
Mariusz Najwer <najwer23@gmail.com>
2025-11-13 21:59:11 +0100

    Mobile menu

--- src/app/navigation/Navigation.tsx
+++ src/app/navigation/Navigation.tsx
@@ -1,8 +1,12 @@
+import { Button } from 'najwer23morsels/lib/button';
 import { Grid } from 'najwer23morsels/lib/grid';
 import { TextBox } from 'najwer23morsels/lib/textbox';
+import { useState } from 'react';
 import styles from './Navigation.module.css';
 
 export const Navigation: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <Grid layout="container" widthMax={1600}>
       <Grid layout="container" widthMax={1400} padding={'20px'} margin={'auto'}>
@@ -17,17 +21,17 @@ export const Navigation: React.FC = () => {
           }}
         >
           <div>
-            <TextBox desktopSize={26} mobileSize={20} fontWeight={500} tag="a" href="/#/">
+            <TextBox desktopSize={20} mobileSize={20} fontWeight={500} tag="a" href="/#/">
               @najwer23
             </TextBox>
           </div>
 
-          <div>
+          <div className={styles.menuItemDesktop}>
             <Grid layout="flex" justifyContent="flex-end">
               <TextBox
                 tag="a"
                 href="https://najwer23.github.io/resume/"
-                desktopSize={26}
+                desktopSize={20}
                 mobileSize={20}
                 fontWeight={500}
                 rel="noreferrer"
@@ -36,6 +40,59 @@ export const Navigation: React.FC = () => {
                 Résumé
               </TextBox>
             </Grid>
+          </div>
+
+          <div className={styles.menuItemMobile}>
+            <div className={styles.menuPlaceholder}>
+              <Button
+                height={'40px'}
+                width={'70px'}
+                padding={0}
+                backgroundColor="orangered"
+                onClick={() => setMenuOpen((prevMenuOpen) => !prevMenuOpen)}
+              >
+                <TextBox tag="span" desktopSize={20} mobileSize={20} fontWeight={400} color="white">
+                  Menu
+                </TextBox>
+              </Button>
+            </div>
+            <div className={`${styles.menu} ${menuOpen ? styles.menuOpen : ''}`}>
+              <div className={styles.menuContent}>
+                <TextBox
+                  tag="a"
+                  href="/#/home"
+                  desktopSize={30}
+                  mobileSize={30}
+                  fontWeight={400}
+                  color="black"
+                  onClick={() => setMenuOpen((prevMenuOpen) => !prevMenuOpen)}
+                >
+                  Home
+                </TextBox>
+                <TextBox
+                  tag="a"
+                  href="/#/about"
+                  desktopSize={30}
+                  mobileSize={30}
+                  fontWeight={400}
+                  color="black"
+                  onClick={() => setMenuOpen((prevMenuOpen) => !prevMenuOpen)}
+                >
+                  About
+                </TextBox>
+                <TextBox
+                  tag="a"
+                  href="https://najwer23.github.io/resume/"
+                  desktopSize={32}
+                  mobileSize={32}
+                  fontWeight={500}
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  Résumé
+                </TextBox>
+              </div>
+            </div>
           </div>
         </Grid>
       </Grid>
