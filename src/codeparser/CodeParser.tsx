40b1055080fe0b1b6f825f28ed90878d504262fb
Mariusz Najwer <najwer23@gmail.com>
2026-04-04 16:22:44 +0200

    fix: adjust styling for code display and improve copy button layout in CodeParser

--- src/codeparser/CodeParser.tsx
+++ src/codeparser/CodeParser.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { CopyButton } from 'najwer23morsels/lib/CopyButton';
 import styles from './CodeParser.module.css';
 
 interface CodeParserProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -8,9 +8,14 @@ interface CodeParserProps extends React.HTMLAttributes<HTMLDivElement> {
 export const CodeParser: React.FC<CodeParserProps> = ({ children, className, ...props }) => {
   return (
     <div className={[styles.n23CodeParser, 'n23CodeParser', className].filter(Boolean).join(' ')} {...props}>
-      <pre>
-        <code>{children}</code>
-      </pre>
+      <div className={styles.codeWrap}>
+        <div className={styles.copyButtonSticky}>
+          <CopyButton textToCopy={String(children)} />
+        </div>
+        <pre>
+          <code>{children}</code>
+        </pre>
+      </div>
     </div>
   );
 };
