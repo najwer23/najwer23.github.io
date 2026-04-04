107bf58cae174367bebe4665cf4a9462a3067d12
Mariusz Najwer <najwer23@gmail.com>
2026-04-04 12:29:22 +0200

    fix: update blog post date and add new content for advanced sorting in JavaScript

--- rss.ts
+++ rss.ts
@@ -1,10 +1,11 @@
 export const rssItems = [
   {
-    title: '#1 My first blog post',
-    link: 'https://najwer23.github.io/#/blog/post/1',
-    guid: '#1-My-first-blog-post',
-    description: 'Hi, I’m Mariusz, and this is my new blog.',
-    pubDate: new Date('2026-03-29T17:00:00+02:00'),
+    title: '#3 Advanced Sorting in JavaScript: Beyond array.sort()',
+    link: 'https://najwer23.github.io/#/blog/post/3',
+    guid: '#3-advanced-sorting-in-javascript-beyond-array-sort',
+    description:
+      'Sorting sounds simple until you try to use it in a real application. Then you run into strings sorted like numbers, objects that need multi-field ordering, locale-sensitive text, and arrays that get mutated when you least expect it. In this post, I’ll show how JavaScript sorting really works, where the common traps are, and how to build more reliable sorting logic for real-world data.',
+    pubDate: new Date('2026-04-07T05:00:00+02:00'),
   },
   {
     title: '#2 Hash Tables in JavaScript: Object, Map and WeakMap',
@@ -13,5 +14,12 @@ export const rssItems = [
     description:
       'JavaScript gives us a few different ways to store and look up key-value data. Object, Map, and WeakMap all solve similar problems, but they do so in slightly different ways. In this article, we’ll look at how each of them works, when to use them, and why Object is often treated as a hash-table-like structure in everyday JavaScript code.',
     pubDate: new Date('2026-04-02T20:00:00+02:00'),
+  },
+  {
+    title: '#1 My first blog post',
+    link: 'https://najwer23.github.io/#/blog/post/1',
+    guid: '#1-My-first-blog-post',
+    description: 'Hi, I’m Mariusz, and this is my new blog.',
+    pubDate: new Date('2026-03-29T17:00:00+02:00'),
   },
 ];
