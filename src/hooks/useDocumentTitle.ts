f0cf72d97a23bc19a1ae8dffbc53b6b51d068828
Mariusz Najwer <najwer23@gmail.com>
2026-04-06 18:10:37 +0200

    ANALYTICS

--- src/hooks/useDocumentTitle.ts
+++ src/hooks/useDocumentTitle.ts
@@ -1,9 +1,25 @@
+import { queryAnalyticsHit } from '@najwer23/app/pages/analytics/Analytics.query';
 import { useEffect } from 'react';
 import ReactGA from 'react-ga4';
 import { useLocation } from 'react-router-dom';
+import { useImmediateThrottledQuery } from './useImmediateThrottledQuery';
 
 export const useDocumentTitle = (title: string) => {
   const location = useLocation();
+  const currentUrl = window.location.href.toLowerCase();
+
+  useImmediateThrottledQuery(
+    {
+      queryKey: ['useDocumentTitle', 'useDocumentTitle' + location.pathname],
+      queryFn: () =>
+        queryAnalyticsHit({ appName: import.meta.env.VITE_ANALYTICS_APP_NAME, pageName: location.pathname }),
+      staleTime: Infinity,
+      gcTime: Infinity,
+      retry: 0,
+      enabled: true, // !currentUrl.includes('localhost'),
+    },
+    0,
+  );
 
   useEffect(() => {
     document.title = title;
