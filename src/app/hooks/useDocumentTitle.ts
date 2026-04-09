d68e22e28007be3af2ac8cec4abe377b095bb963
Mariusz Najwer <najwer23@gmail.com>
2026-04-09 16:38:37 +0200

    New trimTrailingSlashes

--- src/app/hooks/useDocumentTitle.ts
+++ src/app/hooks/useDocumentTitle.ts
@@ -7,11 +7,22 @@ export const useDocumentTitle = (title: string) => {
   const location = useLocation();
   const currentUrl = window.location.href.toLowerCase();
 
+  const trimTrailingSlashes = (path: string) => {
+    const normalized = path.replace(/\/{2,}/g, '/').replace(/\/+$/, '');
+    return normalized === '' ? '/' : normalized;
+  };
+
   useImmediateThrottledQuery(
     {
-      queryKey: ['useDocumentTitle', 'useDocumentTitle' + location.pathname],
+      queryKey: [
+        'useDocumentTitle',
+        'useDocumentTitle' + trimTrailingSlashes(import.meta.env.BASE_URL + location.pathname),
+      ],
       queryFn: () =>
-        queryAnalyticsHit({ appName: import.meta.env.VITE_ANALYTICS_APP_NAME, pageName: location.pathname }),
+        queryAnalyticsHit({
+          appName: import.meta.env.VITE_ANALYTICS_APP_NAME,
+          pageName: trimTrailingSlashes(import.meta.env.BASE_URL + location.pathname),
+        }),
       staleTime: 0,
       gcTime: 0,
       retry: 0,
